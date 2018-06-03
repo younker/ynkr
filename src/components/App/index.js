7477465dce8345fc4602c022cc3acc4a1f768ae5
Jason Younker <jason@ynkr.org>
2018-06-02 22:26:27 -0700

    add silly little saying

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -7,6 +7,10 @@ class App extends Component {
     return (
       <div className="App">
         <Navbar />
+        <div style={{ float: 'right', marginRight: '10px' }}>
+          The Cobbler's children have no shoes...and I don't have a real
+          website.
+        </div>
         <Terminal />
       </div>
     );
