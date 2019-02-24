9f0d8e5856a4132c303530c4b55f7f2649588259
Jason Younker <jason@ynkr.org>
2019-02-24 01:09:50 -0800

    env util to help us understand where we are. This will come in handy when we hook up ci and make our app more stage-aware.

---
+++ src/util/env.js
@@ -0,0 +1,19 @@
+import R from 'ramda';
+
+// We are stage-less (other than test) so this should work
+const DEFAULT_ENV = 'production';
+
+const NORMALIZED_ENV = R.pathOr(DEFAULT_ENV, ['env', 'NODE_ENV'], process).toLowerCase();
+
+// By using this predicate internally and exposing functions with 0 arity, we
+// we reduce the liklihood of errors such as the following:
+//   > const NORMALIZED_ENV = 'production';
+//   > const isEnv = (env) => NORMALIZED_ENV === env;
+//   > isEnv('prod')
+//   false
+const isEnv = (env) => NORMALIZED_ENV === env;
+
+export default {
+  isTest: isEnv('test'),
+  isProd: isEnv('production'),
+};
